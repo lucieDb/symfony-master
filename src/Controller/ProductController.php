3a723ff8bb0dc77896507d958f94b75fd010c66f
Lucie.dbb <debrabanderelucie@gmail.com>
2020-09-18 12:03:39 +0200

    v2

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/product/create", name="product_create")
      */
-    public function index()
+    public function create(Request $request, EntityManagerInterface $entityManager)
     {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product); // on persiste l'objet
+            $entityManager->flush(); // on exécute la requête (INSERT...)
+        }
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'form' => $form->createView(),
         ]);
     }
 }
